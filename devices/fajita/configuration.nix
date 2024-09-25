2addd00206b41507ab4e599277ef2b3db2af7557
gmicroul <121873438+gmicroul@users.noreply.github.com>
2024-09-25 22:59:04 +0800

    Add files via upload

--- devices/fajita/configuration.nix
+++ devices/fajita/configuration.nix
@@ -1,25 +1,65 @@
+{ config, lib, pkgs, ... }:
+
+let
+  inherit (lib) mkForce;
+  system_type = config.mobile.system.type;
+
+  defaultUserName = "alice";
+in
 {
-  config,
-  pkgs,
-  mobileNixos,
-  ...
-}:
+  imports = [
+    ./phosh.nix
+  ];
 
-{
-  imports = [ ];
+  config = lib.mkMerge [
+    # INSECURE STUFF FIRST
+    # Users and hardcoded passwords.
+    {
+      # Forcibly set a password on users...
+      # Note that a numeric password is currently required to unlock a session
+      # with the plasma mobile shell :/
+      users.users.${defaultUserName} = {
+        isNormalUser = true;
+        # Numeric pin makes it **possible** to input on the lockscreen.
+        password = "1234";
+        home = "/home/${defaultUserName}";
+        extraGroups = [
+          "dialout"
+          "feedbackd"
+          "networkmanager"
+          "video"
+          "wheel"
+        ];
+        uid = 1000;
+      };
 
-  # Basic system settings
-  system.stateVersion = "23.05"; # Adjust to your nixpkgs version
+      users.users.root.password  = "nixos";
+      mobile.quirks.qualcomm.sdm845-modem.enable = true;
+      system.stateVersion = "24.05";
+      # Automatically login as defaultUserName.
+      services.xserver.displayManager.autoLogin = {
+        user = defaultUserName;
+      };
+      services.xserver.desktopManager.phosh = {
+        user = defaultUserName;
+      };
+    }
 
-  # Example of additional configuration
-  networking.hostName = "fajita";
+    # Networking, modem and misc.
+    {
+      # Ensures any rndis config from stage-1 is not clobbered by NetworkManager
+      networking.networkmanager.unmanaged = [ "rndis0" "usb0" ];
 
-  users.users.root = {
-    password = "root";
-  };
+      # Setup USB gadget networking in initrd...
+      mobile.boot.stage-1.networking.enable = lib.mkDefault true;
+    }
 
-  services.openssh.enable = true;
-
-  # Mobile NixOS settings
-  mobile.boot.stage-1.splash.enable = true;
+    # SSH
+    {
+      # Start SSH by default...
+      # Not a good idea given the fact this config is insecure (well-known password).
+      services.openssh.enable = true;
+    }
+  ];
 }
+
---
+++ devices/fajita/phosh.nix
@@ -0,0 +1,49 @@
+#
+# This file represents safe opinionated defaults for a basic Phosh system.
+#
+# NOTE: this file and any it imports **have** to be safe to import from
+#       an end-user's config.
+#
+{ config, lib, pkgs, options, ... }:
+
+{
+  mobile.beautification = {
+    silentBoot = lib.mkDefault true;
+    splash = lib.mkDefault true;
+  };
+
+  services.xserver.desktopManager.phosh = {
+    enable = true;
+    group = "users";
+  };
+
+  programs.calls.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    firefox
+    vim
+    git
+    wget
+    mpv
+    podman
+    nmon
+    htop
+    cmus
+    neofetch
+    kgx                 # Terminal
+  ];
+
+  hardware.sensor.iio.enable = true;
+  sound.enable = true;
+  nixpkgs.config.pulseaudio = true;
+  hardware.pulseaudio.enable = true;
+
+  assertions = [
+    { assertion = options.services.xserver.desktopManager.phosh.user.isDefined;
+    message = ''
+      `services.xserver.desktopManager.phosh.user` not set.
+        When importing the phosh configuration in your system, you need to set `services.xserver.desktopManager.phosh.user` to the username of the session user.
+    '';
+    }
+  ];
+}
